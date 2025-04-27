019defbed3ceecf4c05e6f521f943d1059b75dcd
GINSAN170 <danjak882@gmail.com>
2025-04-27 15:26:55 +0200

    Add files via upload

---
+++ Waybar/cava-wrapper.sh
@@ -0,0 +1,21 @@
+
+BLOCKS=("▂" "▂" "▃" "▃" "▄" "▄" "▆" "▆" "█")
+
+cava -p ~/.config/cava/cava.conf | while IFS= read -r line; do
+
+    line=${line%;}
+    
+
+    IFS=';' read -ra values <<< "$line"
+    
+
+    output=""
+    for v in "${values[@]}"; do
+
+        v=$(( v > 8 ? 8 : v ))
+        output+="${BLOCKS[$v]}"
+    done
+    
+   
+    echo "$output"
+done
