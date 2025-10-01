634fb844b9ccb0e3ce6727447308128941a0cd34
GINSAN170 <danjak882@gmail.com>
2025-10-01 23:39:41 +0200

    update nvim config

--- nvim/lua/Dao/settings/options.lua
+++ nvim/lua/Dao/settings/options.lua
@@ -1,5 +1,3 @@
-vim.cmd("let g:netrw_banner = 0")
-
 vim.opt.guicursor = ""
 vim.opt.nu = true
 vim.opt.tabstop = 8
@@ -9,7 +7,6 @@ vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.wrap = true
-vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undofile = true
 vim.opt.incsearch = true
@@ -18,12 +15,11 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.termguicolors = true
 vim.opt.background = "dark"
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 10
 vim.opt.signcolumn = "yes"
-vim.opt.backspace = {"start", "eol", "indent"}
-vim.opt.isfname:append("@-@")
-vim.opt.updatetime = 50
-vim.opt.colorcolumn = "80"
+vim.opt.backspace = { "start", "eol", "indent" }
+vim.opt.splitright = true
+vim.opt.splitbelow = true
 vim.opt.clipboard:append("unnamedplus")
 vim.opt.hlsearch = true
 vim.g.editorconfig = true
